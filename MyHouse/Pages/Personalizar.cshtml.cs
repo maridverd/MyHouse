d0fff62492eca281a05475062deb9a502a67bbd3
EllenTorres1 <ellentorres2012@gmail.com>
2025-06-10 20:19:19 -0300

    teste

--- MyHouse/Pages/Personalizar.cshtml.cs
+++ MyHouse/Pages/Personalizar.cshtml.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 public class PersonalizarModel : PageModel
 {
-    // Propriedade para receber o caminho da imagem
-    [BindProperty(SupportsGet = true)]
+    [BindProperty]
     public string ImagemCasa { get; set; }
+
+    [BindProperty]
+    public List<string> Personalizacao { get; set; } = new List<string>();
 
     public void OnGet()
     {
-        // Você pode inicializar algo aqui se precisar
+        // Pode deixar vazio ou carregar dados caso queira
+    }
+
+    public IActionResult OnPost()
+    {
+        // Aqui você pode salvar os dados da personalização se quiser
+        // Exemplo: salvar no banco, sessão, etc.
+
+        // Após salvar, redireciona para a Index com mensagem
+        return RedirectToPage("/Index", new { mensagem = "Personalização salva" });
     }
 }
