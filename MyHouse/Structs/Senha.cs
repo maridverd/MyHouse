317d8e6807f2cc6c293c492ba5f0e273923c2fde
Amarelo1 <pedrocourap@gmail.com>
2025-06-11 08:42:04 -0300

    Hash na senha adicionado e Modelo aportuguesado.

--- MyHouse/Structs/Senha.cs
+++ MyHouse/Structs/Senha.cs
@@ -1,20 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
 
-public readonly struct Senha
-{
-    private readonly string _Senha;
+namespace MyHouse;
 
-    public Senha(string senha)
-    {
-        _Senha = senha;
+public readonly struct Senha {
+    [JsonInclude]
+    public readonly string HashSenha;
+    [JsonInclude]
+    public readonly string Sal;
+    public Senha(string senha) {
+        Sal = Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+        senha += Sal;
+        HashSenha = Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(senha)));
     }
-
-    public override string ToString() => _Senha;
-
-    public override bool Equals(object? obj) => obj is Senha other && _Senha == other._Senha;
-    public override int GetHashCode() => _Senha.GetHashCode();
-
-    public static implicit operator Senha(string senha) => new(senha);
-    public static implicit operator string(Senha senha) => senha._Senha;
-
-    public bool Valida => _Senha.Length >= 6; // Exemplo de validaÁ„o
+    public readonly bool Validate(string senha) {
+        return Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(senha + Sal))) == HashSenha;
+    }
+    public static bool Valida(string senha) => senha.Length >= 6; // Exemplo de valida√ß√£o
 }
